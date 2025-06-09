f997720a9e8b615c972aa21a05d1b803fa3b787a
AngelBalmori <angelbalmori23@gmail.com>
2025-06-09 17:29:06 -0600

    script de creación de tablas finales

---
+++ sql/crear_tablas_finales.sql
@@ -0,0 +1,14 @@
+Script para crear las tablas utilizadas en el modelo final
+
+CREATE TABLE inflacion (
+    pais VARCHAR(100),
+    anio INT,
+    valor FLOAT
+);
+
+CREATE TABLE terrorismo (
+    pais VARCHAR(100),
+    anio INT,
+    indice FLOAT
+);
+
