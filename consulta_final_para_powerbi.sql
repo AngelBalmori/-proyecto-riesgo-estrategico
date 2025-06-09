5b45211a83725c5ea3c4fa7bc6d90fc0484a0890
AngelBalmori <angelbalmori23@gmail.com>
2025-06-09 17:50:57 -0600

    Update consulta_final_para_powerbi.sql

--- consulta_final_para_powerbi.sql
+++ consulta_final_para_powerbi.sql
@@ -1,15 +1,1 @@
--- Consulta final exportada a Power BI
-
-SELECT 
-    pais,
-    anio,
-    inflacion,
-    inversion_extranjera,
-    terrorismo,
-    militarizacion,
-    paz_interna,
-    pib,
-    deuda_externa
-FROM riesgo_estrategico_2030
-WHERE anio BETWEEN 2010 AND 2023
-ORDER BY pais, anio;
+Eliminar archivo duplicado fuera de la carpeta sql
